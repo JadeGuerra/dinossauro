0b3517020ec293dfb01efc47016fd4578efa7040
Jade Uliano Guerra <jug.guerra@MacBook-Pro-de-Jade.local>
2020-10-21 14:45:53 -0300

    first commit

---
+++ dino-game/script.js
@@ -0,0 +1,32 @@
+const dino = document.querySelector('.dino');
+
+function handleKeyUp(event){
+    if(event.keyCode === 32){
+        jump();
+    }
+}
+
+function jump(){
+    let position = 0;
+    let upInterval = setInterval(()=>{
+        if (position >= 150){
+            //aqui para
+            clearInterval(upInterval);
+
+            //aqui desce
+            let downInterval = setInterval(() => {
+                if (position <= 0){
+                    clearInterval(downInterval);
+                }else{
+                position -= 20;
+                dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        }else{
+            //aqui sobe
+        position += 20;
+        dino.style.bottom = position + 'px';
+        }
+    }, 20);
+}
+document.addEventListener('keyup', handleKeyUp);
