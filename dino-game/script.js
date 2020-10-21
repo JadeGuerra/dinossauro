6f4c428b84be071e0a1cc3b2cdfd1ee7d1478f04
Jade Uliano Guerra <jug.guerra@MacBook-Pro-de-Jade.local>
2020-10-21 16:02:26 -0300

    dino pula cactus

--- dino-game/script.js
+++ dino-game/script.js
@@ -4,6 +4,8 @@ const background = document.querySelector('.background');
 
 let isJumping = false;
 
+let position = 0;
+
 function handleKeyUp(event){
     if(event.keyCode === 32){
         if (!isJumping){
@@ -13,7 +15,6 @@ function handleKeyUp(event){
 }
 
 function jump(){
-    let position = 0;
     isJumping = true;
     let upInterval = setInterval(() => {
         if (position >= 150){
@@ -41,10 +42,28 @@ function jump(){
 function createCactus(){
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        
+        if (cactusPosition < -60){
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            //game over
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">Fim do jogo</h1>';
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
 }
 
 createCactus();
